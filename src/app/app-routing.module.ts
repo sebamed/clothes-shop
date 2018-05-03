5286ba888840f55b8a84fce2135bb9cb005d76fa
Sebastian Dudas <sebastian.frontend@gmail.com>
2018-05-03 21:52:04 +0200

    added routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages' },
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routing
+import { AppRoutingModule } from './app-routing.module';
 
+// components
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
