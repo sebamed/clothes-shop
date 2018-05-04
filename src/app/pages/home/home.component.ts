4dc90662f031a37a5be6bdf6da096d3bb1de605d
Sebastian Dudas <sebastian.frontend@gmail.com>
2018-05-04 23:44:28 +0200

    fixed headers

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,10 +14,10 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.carouselOne = {
         grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
         slide: 1,
-        speed: 400,
+        speed: 700,
         interval: 4000,
         point: {
-          visible: true
+          visible: false
         },
         load: 2,
         touch: true,
