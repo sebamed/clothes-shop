1ebda4c75d0ad3e4ad50c0d5f652897e913ca3ee
Sebastian Dudas <sebastian.frontend@gmail.com>
2018-05-04 16:35:59 +0200

    temp navbar

--- src/app/pages/shared/menu/menu.component.ts
+++ src/app/pages/shared/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
+
+declare var $ : any;
+
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
@@ -7,12 +10,22 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class MenuComponent implements OnInit, OnDestroy {
 
+    search: boolean = true;
 
     ngOnInit() {
-
+        $('.search-form').hide();
     }
 
     ngOnDestroy() {
 
+    }
+
+    toggleSearch(){
+        if(this.search){
+            $('.search-form').show("slide", { direction: "right" }, 500);
+        } else {
+            $('.search-form').hide("slide", { direction: "right" }, 500);
+        }
+        this.search = !this.search;
     }
 }
