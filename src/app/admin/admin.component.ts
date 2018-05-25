04979a1e3b2474e65d48ae0e28297b1777706abb
Sebastian Dudas <sebastian.frontend@gmail.com>
2018-05-25 20:48:56 +0200

    admin module with authorization

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { ToastSerice } from '../pages/shared/toast/toast.service';
+import { ToastTitle } from '../model/enum/toast-title.enum';
+import { ToastType } from '../model/enum/toast-type.enum';
+import { ToastIcon } from '../model/enum/toast-icon.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'admin',
   template: '<router-outlet></router-outlet>'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit, OnDestroy {
+
+  constructor(private _user: UserService,
+    private _toasts: ToastSerice,
+    private _router: Router
+  ) {
+
+  }
+
+  ngOnInit() {
+    // provera da li je ulogovani user admin i da li postoji ulogovani user
+    if (this._user.getCurrentUser() === undefined || this._user.getCurrentUser().role.name != 'admin') {
+      // nije admin
+      this._toasts.addToast({
+        title: ToastTitle.warning,
+        message: "You are not authorized to be here, so we are redirecting you!",
+        type: ToastType.warning,
+        icon: ToastIcon.warning
+      });
+      this._router.navigate(['/home']);
+    }
+  }
+
+  ngOnDestroy() {
+    // 
+  }
 
 }
